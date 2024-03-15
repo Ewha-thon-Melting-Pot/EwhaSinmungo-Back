2351e01ad46805d172ed1a6fcffdede1796bad96
Lee Hannah <mohan0226@naver.com>
2024-03-15 19:48:55 +0900

    project setting

---
+++ src/main/java/melting_pot/ewha_sinmungo/EwhaSinmungoApplication.java
@@ -0,0 +1,13 @@
+package melting_pot.ewha_sinmungo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EwhaSinmungoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EwhaSinmungoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/melting_pot/ewha_sinmungo/EwhaSinmungoApplicationTests.java
@@ -0,0 +1,13 @@
+package melting_pot.ewha_sinmungo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EwhaSinmungoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
