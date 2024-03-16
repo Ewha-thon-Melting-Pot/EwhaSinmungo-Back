59d0c00042ba26190458a4352ffc51bcf8d92cb2
Lee Hannah <mohan0226@naver.com>
2024-03-16 15:59:58 +0900

    fix: 시큐리티 접근 권한 수정

--- src/main/java/melting_pot/ewha_sinmungo/global/config/SecurityConfig.java
+++ src/main/java/melting_pot/ewha_sinmungo/global/config/SecurityConfig.java
@@ -6,9 +6,7 @@ import melting_pot.ewha_sinmungo.global.jwt.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -31,10 +29,9 @@ public class SecurityConfig{
                 .and()
                 .authorizeHttpRequests()
                 // 해당 API에 대해서는 모든 요청을 허가
-                .antMatchers("/auth/login").permitAll()
-                .antMatchers("/auth/signup").permitAll()
+                .antMatchers("/auth/login", "/auth/signup").permitAll()
                 // USER 권한이 있어야 요청할 수 있음
-                .antMatchers("/auth/test").hasRole("GUEST")
+                .antMatchers("/admin/**").hasRole("ADMIN")
                 // 이 밖에 모든 요청에 대해서 인증을 필요로 한다는 설정
                 .anyRequest().authenticated()
                 .and()
