7827d2b861c04c2734da52fe8db750765bf61de2
Lee Hannah <mohan0226@naver.com>
2024-03-17 13:27:16 +0900

    fix: 기존 투표 코드 삭제

--- src/main/java/melting_pot/ewha_sinmungo/post/repository/PostMemberReactionRepository.java
+++
@@ -1,8 +0,0 @@
-package melting_pot.ewha_sinmungo.post.repository;
-
-import melting_pot.ewha_sinmungo.post.domain.PostMemberReaction;
-import melting_pot.ewha_sinmungo.post.domain.PostMemberReactionId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostMemberReactionRepository extends JpaRepository<PostMemberReaction, PostMemberReactionId> {
-}
